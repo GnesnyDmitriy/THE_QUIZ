4f415ab43a98e4d0223828ff750453a693d3f2b4
Margarita Bespalova <margo.ivi@yandex.ru>
2023-03-05 16:52:38 +0300

    correction of tablen layout

---
+++ app/src/main/java/com/practicum/quiz_version_4/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.practicum.quiz_version_4
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        hideSystemUI()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainButton = findViewById<ImageView>(R.id.main_button)
+
+
+
+
+        mainButton.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.systemBarsBehavior = BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        insetsController.hide((WindowInsetsCompat.Type.systemBars()))
+    }
+}
